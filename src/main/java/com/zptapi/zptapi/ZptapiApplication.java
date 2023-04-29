90af8b7589f32ee89ef22f32c26e62da4739ec0a
Alt-az <235979@edu.p.lodz.pl>
2023-04-29 21:56:52 +0200

    init

---
+++ src/main/java/com/zptapi/zptapi/ZptapiApplication.java
@@ -0,0 +1,13 @@
+package com.zptapi.zptapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZptapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZptapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zptapi/zptapi/ZptapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zptapi.zptapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZptapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
