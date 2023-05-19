22cbb1d8a36f98b58e80767043bf9a0183fc0d3c
Alt-az <235979@edu.p.lodz.pl>
2023-05-19 10:59:30 +0200

    turning off database init

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,16 @@
-CREATE TABLE IF NOT EXISTS diet_plan(
-    `id` bigint(20) NOT NULL,
-    PRIMARY KEY (`id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-CREATE TABLE IF NOT EXISTS excercise_plan(
-    `id` bigint(20) NOT NULL,
-    PRIMARY KEY (`id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-CREATE TABLE IF NOT EXISTS clients(
-    `id` bigint(20) NOT NULL AUTO_INCREMENT,
-    `email` varchar(255) DEFAULT NULL,
-    `password` varchar(255) DEFAULT NULL,
-    `weight` int DEFAULT NULL,
-    `height` int DEFAULT NULL,
-    PRIMARY KEY (`id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+--CREATE TABLE IF NOT EXISTS diet_plan(
+--    `id` bigint(20) NOT NULL,
+--    PRIMARY KEY (`id`)
+--)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+--CREATE TABLE IF NOT EXISTS excercise_plan(
+--    `id` bigint(20) NOT NULL,
+--    PRIMARY KEY (`id`)
+--)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+--CREATE TABLE IF NOT EXISTS clients(
+--    `id` bigint(20) NOT NULL AUTO_INCREMENT,
+--    `email` varchar(255) DEFAULT NULL,
+--    `password` varchar(255) DEFAULT NULL,
+--    `weight` int DEFAULT NULL,
+--    `height` int DEFAULT NULL,
+--    PRIMARY KEY (`id`)
+--)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
